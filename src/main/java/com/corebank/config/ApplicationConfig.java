6baa36e1922b86c119e3e617c416a990b321f732
JoaoFerreira-1994 <jpsferreira1994@gmail.com>
2026-09-11 09:29:15 +0100

    V1.0 - Initial project structure and Jakarta REST configuration

---
+++ src/main/java/com/corebank/config/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package com.corebank.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {
+}
